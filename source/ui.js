db416c5f1aa5e6e2b7c1d73e86972ab482687825
Qiu <qiu.gits@gmail.com>
2018-05-21 21:31:16 +0900

    Update to qiugits

--- source/ui.js
+++ source/ui.js
@@ -20,39 +20,23 @@ const handleSelect = item => {
 const items = [
 	{
 		label: 'Website',
-		url: 'https://sindresorhus.com'
+		url: 'https://gitshell.net'
 	},
 	{
 		label: 'Twitter',
-		url: 'https://twitter.com/sindresorhus'
+		url: 'https://twitter.com/qiugits'
 	},
 	{
 		label: 'GitHub',
-		url: 'https://github.com/sindresorhus'
+		url: 'https://github.com/qiugits'
 	},
 	{
-		label: 'Blog',
-		url: 'https://blog.sindresorhus.com'
+		label: 'Qiita',
+		url: 'https://qiita.com/qiugits'
 	},
 	{
 		label: 'Ask Me Anything',
-		url: 'https://github.com/sindresorhus/ama'
-	},
-	{
-		label: 'Contact',
-		url: 'https://sindresorhus.com/contact'
-	},
-	{
-		label: 'Support my open source work',
-		url: 'https://www.patreon.com/sindresorhus'
-	},
-	{
-		label: 'Unicorns!',
-		async action() {
-			console.log(await terminalImage.file(path.join(__dirname, 'unicorn1.gif')));
-			console.log(await terminalImage.file(path.join(__dirname, 'unicorn2.gif')));
-			console.log(await terminalImage.file(path.join(__dirname, 'unicorn3.gif')));
-		}
+		url: 'https://github.com/qiugits/ama'
 	},
 	// TODO: Add separator item here when https://github.com/vadimdemedes/ink-select-input/issues/4 is done
 	{
@@ -67,7 +51,7 @@ module.exports = () => (
 	<div>
 		<br/>
 		<div>
-			<Text>I’m a full-time open-sourcerer making things like macOS apps, CLI tools, and modules.</Text>
+			<Text>I’m an undergraduate student, interested in Machine Learning, Engineering, and Human Mind, love Anime, Manga, and Movies.</Text>
 		</div>
 		<br/>
 		<SelectInput items={items} onSelect={handleSelect}/>
